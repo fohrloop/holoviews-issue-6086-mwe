545c74cba110249cbc66554a350d6371f7f60ad5
Niko Föhr <fohrloop@gmail.com>
2024-02-03 18:26:40 +0200

    prune the conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -3,14 +3,5 @@
 
 from nbsite.shared_conf import *
 
-
-master_doc = "index"
 project = "MWE docs"
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 nitpicky = True
-
-
-extensions += [
-    "sphinx_copybutton",
-    "nbsite.analytics",
-]
